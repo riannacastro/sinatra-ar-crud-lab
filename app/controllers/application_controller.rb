ff5aefef469a7f2d55093bb132cc72cacb6651e7
Rianna Castro <riannaxcastro@gmail.com>
2021-05-27 13:20:02 -1000

    commit tree 47456b0d5b237a106a5af9dba137b15c1214d7bd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,44 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+ 
+  get '/articles/new' do
+    @article = Article.new
+    erb :new
+  end
+  
+  post '/articles' do
+    @article = Article.new(params)
+    @article.save
+    redirect "/articles/#{@article.id}"
+  end
+  
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+  
+    @article.update(params[:article])
+
+    redirect "/articles/#{@article.id}"
+  end
+  
+  delete '/articles/:id' do
+    Article.delete(params[:id])
+    redirect "/articles"
+  end
+ 
 end
